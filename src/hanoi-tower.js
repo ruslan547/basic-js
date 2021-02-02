6ea63f32f91a9b5338fa24c21ceb1c00cc9d6fd7
ruslannovik <novik.rv93@gmail.com>
2021-02-02 15:57:51 +0300

    feat: add calculateHanoi() in the hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
+const SECOND_PER_HOUR = 3600;
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor(turns / turnsSpeed * SECOND_PER_HOUR);
+
+  return { turns, seconds };
 };
