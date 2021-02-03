f5bbbac48d69cd5dbe7bdb31b0d3172f4e095d48
ruslannovik <novik.rv93@gmail.com>
2021-02-03 19:57:33 +0300

    feat: implement the class VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,11 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
+const A_CODE = 'a'.charCodeAt();
+const Z_CODE = 'z'.charCodeAt();
+
 class VigenereCipheringMachine {
   constructor(modification = true) {
     this.modification = modification;
   }
-  encrypt(message, key) {
 
+  encrypt(message, key) {
+    return this.playMachine(message, key, 'encrypt');
+  }
+  decrypt(encryptedMessage, key) {
+    return this.playMachine(encryptedMessage, key, 'decrypt');
+  }
+
+  playMachine(message, key, type) {
     if (!(message && key)) {
       throw new Error();
     }
@@ -14,38 +24,46 @@ class VigenereCipheringMachine {
     key = String(key).toLowerCase();
 
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    let result = '';
+    let result = [];
     let j = 0;
 
     for (let i = 0; i < message.length; i++) {
       if (!alphabet.includes(message[i])) {
-        result += message[i];
+        result.push(message[i]);
         continue;
       }
 
-      const aCode = 'a'.charCodeAt();
-      const zCode = 'z'.charCodeAt();
-      const k = key.charCodeAt(j) - aCode;
+      const k = key.charCodeAt(j) - A_CODE;
       const mesCode = message.charCodeAt(i);
 
       let resCode;
 
-      if (mesCode + k > zCode) {
-        resCode = (mesCode - aCode + k) % 26 + aCode;
+      if (type === 'encrypt') {
+        if (mesCode + k > Z_CODE) {
+          resCode = (mesCode - A_CODE + k) % 26 + A_CODE;
+        } else {
+          resCode = mesCode + k;
+        }
+      } else if (type === 'decrypt') {
+        if (mesCode - k < A_CODE) {
+          resCode = mesCode + 26 - k;
+        } else {
+          resCode = mesCode - k;
+        }
       } else {
-        resCode = mesCode + k;
+        throw new Error();
       }
 
-      result += String.fromCharCode(resCode);
+      result.push(String.fromCharCode(resCode));
       j++;
       j %= key.length;
     }
 
-    return result.toUpperCase();
-  }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.modification) {
+      result.reverse();
+    }
+
+    return result.join('').toUpperCase();
   }
 }
 
