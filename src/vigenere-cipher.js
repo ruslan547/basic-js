109cecfe21d930e9e5a54ac91497e784471b519e
ruslannovik <novik.rv93@gmail.com>
2021-02-03 12:29:26 +0300

    feat: implement the function getSeason(date)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,10 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
+  constructor(modification = true) {
+    this.modification = modification;
+  }
+  encrypt(message, key) {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
+    if (!(message && key)) {
+      throw new Error();
+    }
+
+    message = String(message).toUpperCase();
+    key = String(key);
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result;
+    let i;
+
+    for (let word of message) {
+      if (i >= key.length) {
+        i = 0;
+      }
+
+      if (alphabet.includes(word)) {
+        result +=
+          i++;
+        continue;
+      }
+
+
+    }
+  }
   decrypt() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    const er = new Error();
+    er._validationProp = 'NA';
+    throw er;
+  }
+
+  const month = date.getMonth();
+
+  if (month > 1 && month < 5) {
+    return 'spring';
+  } else if (month > 4 && month < 8) {
+    return 'summer';
+  } else if (month > 7 && month < 11) {
+    return 'autumn';
+  } else {
+    return 'winter';
+  }
 };
