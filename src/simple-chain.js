fba3e6c279c1925aafafd61826c498c88190f8d5
ruslannovik <novik.rv93@gmail.com>
2021-02-03 08:45:13 +0300

    feat: create obj chainMarker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (
+      isNaN(position) ||
+      position % 1 ||
+      position < 0 ||
+      position >= this.getLength()
+    ) {
+      this.chain = [];
+      const er = new Error();
+      er._validationProp = 'NA';
+      throw er;
+    }
+
+    this.chain.splice(--position, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+
+    this.chain.forEach((item, index) => {
+      result += index ? `~~( ${item} )` : `( ${item} )`;
+    });
+    this.chain = [];
+
+    return result;
   }
 };
 
