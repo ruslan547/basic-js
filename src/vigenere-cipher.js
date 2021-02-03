7eeb3024fedf721280339067b6d1f5dfa726e76a
ruslannovik <novik.rv93@gmail.com>
2021-02-03 13:40:31 +0300

    feat: implement encrypt(message, key)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,32 +5,43 @@ class VigenereCipheringMachine {
     this.modification = modification;
   }
   encrypt(message, key) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
     if (!(message && key)) {
       throw new Error();
     }
 
-    message = String(message).toUpperCase();
-    key = String(key);
+    message = String(message).toLowerCase();
+    key = String(key).toLowerCase();
 
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let result;
-    let i;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    let j = 0;
 
-    for (let word of message) {
-      if (i >= key.length) {
-        i = 0;
-      }
-
-      if (alphabet.includes(word)) {
-        result +=
-          i++;
+    for (let i = 0; i < message.length; i++) {
+      if (!alphabet.includes(message[i])) {
+        result += message[i];
         continue;
       }
 
+      const aCode = 'a'.charCodeAt();
+      const zCode = 'z'.charCodeAt();
+      const k = key.charCodeAt(j) - aCode;
+      const mesCode = message.charCodeAt(i);
 
+      let resCode;
+
+      if (mesCode + k > zCode) {
+        resCode = (mesCode - aCode + k) % 26 + aCode;
+      } else {
+        resCode = mesCode + k;
+      }
+
+      result += String.fromCharCode(resCode);
+      j++;
+      j %= key.length;
     }
+
+    return result.toUpperCase();
   }
   decrypt() {
     throw new CustomError('Not implemented');
